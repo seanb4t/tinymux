d6feec2f021e46d64415c36756f34fa0ab9782f4
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-01-17 23:56:15 +0000

    Merged dev_jake r879:893 to dev.
     -- Make mux_string copy constructor 'const mux_string &'.
     -- Delete msgFinal in the correct scope.
     -- mux_string::export_TextPlain wasn't null-terminating.
     -- Change notify_check to use mux_string, and match listen and filter
        patterns against the non-ansi portion of the string. (Issue 273)
     -- This should not change other behavior, but may require extra review
        before promotion.
     -- Add mux_string append, import, and prepend methods for dbrefs.
     -- Add mux_string & overload of raw_notify_html.
     -- Add mux_string & overload of raw_notify.
     -- Add mux_string & overload of queue_string.
     -- Add iEndGoal argument to mux_string::export_TextAnsi, allowing output in nobleed mode.
     -- Make bufc optional for mux_string:export_TextAnsi and export_TextPlain.
     -- Change use of mux_string * in parameters to mux_string &.
     -- Use const more in mux_string member functions.

--- mux/src/functions.cpp
+++ mux/src/functions.cpp
@@ -6792,7 +6792,7 @@ static FUNCTION(fun_edit)
     mux_string *sFrom = new mux_string(fargs[1]);
     mux_string *sTo   = new mux_string(fargs[2]);
 
-    sStr->edit(sFrom, sTo);
+    sStr->edit(*sFrom, *sTo);
     sStr->export_TextAnsi(buff, bufc);
 
     delete sStr;
@@ -8698,10 +8698,7 @@ static FUNCTION(fun_wrap)
     size_t nStr = sStr->length();
 
     char *pPlain = alloc_lbuf("fun_wrap.pPlain");
-    char *pcPlain = pPlain;
-
     char *pColor = alloc_lbuf("fun_wrap.pColor");
-    char *pcColor = pColor;
 
     size_t nLength = 0;
     bool newline = false;
@@ -8710,13 +8707,11 @@ static FUNCTION(fun_wrap)
 
     while (nPos < nStr)
     {
-        pcPlain = pPlain;
-        sStr->export_TextPlain(pPlain, &pcPlain, nPos);
+        sStr->export_TextPlain(pPlain, NULL, nPos);
 
         nLength = wraplen(pPlain, nPos == 0 ? nFirstWidth : nWidth, newline);
 
-        pcColor = pColor;
-        sStr->export_TextAnsi(pColor, &pcColor, nPos, nLength);
+        sStr->export_TextAnsi(pColor, NULL, nPos, nLength);
 
         if (0 != nPos)
         {
@@ -9833,7 +9828,7 @@ size_t transform_range(mux_string &sStr)
                     sTemp->append(cBefore);
                     cBefore++;
                 }
-                sStr.replace_Chars(sTemp, nStart, 1);
+                sStr.replace_Chars(*sTemp, nStart, 1);
             }
             else if (  mux_islower(cBefore)
                     && mux_isupper(cAfter))
@@ -9850,7 +9845,7 @@ size_t transform_range(mux_string &sStr)
                     sTemp->append(cBefore);
                     cBefore++;
                 }
-                sStr.replace_Chars(sTemp, nStart, 1);
+                sStr.replace_Chars(*sTemp, nStart, 1);
             }
         }
         else if (  mux_isdigit(cBefore)
@@ -9865,7 +9860,7 @@ size_t transform_range(mux_string &sStr)
                 sTemp->append(cBefore);
                 cBefore++;
             }
-            sStr.replace_Chars(sTemp, nStart, 1);
+            sStr.replace_Chars(*sTemp, nStart, 1);
         }
         nStart++;
         bSucceeded = sStr.search("-", &nPos, nStart);
