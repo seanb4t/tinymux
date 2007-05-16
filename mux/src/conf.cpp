53596de2bc8bdaf083c437af982a6971bbed3b94
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-05-16 14:51:24 +0000

    -- Add pipepump() to handle blocking RPC to stubslave.

--- mux/src/conf.cpp
+++ mux/src/conf.cpp
@@ -1782,6 +1782,10 @@ static CF_HAND(cf_module)
         UTF8 *filename = buffer;
 #endif
         mr = mux_AddModule(str, filename);
+#if defined(STUB_SLAVE)
+        // TODO: Send instructions to stubslave to add the same module.
+        //
+#endif
         free_lbuf(buffer);
     }
 
