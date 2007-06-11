71af832bcf1eb42b2aecd9d5bd525a94d55f6f3c
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-06-11 07:26:30 +0000

    -- Allow %=<name> to support single-letter attribute names and document. Issue 341.

--- mux/src/eval.cpp
+++ mux/src/eval.cpp
@@ -1970,8 +1970,7 @@ void mux_exec( const UTF8 *pStr, size_t nStr, UTF8 *buff, UTF8 **bufc, dbref exe
                                 memcpy(mux_scratch, pStr + iStr, n);
                                 mux_scratch[n] = '\0';
 
-                                if (  mux_isattrnameinitial(mux_scratch)
-                                   && '\0' != *utf8_NextCodePoint(mux_scratch))
+                                if (mux_isattrnameinitial(mux_scratch))
                                 {
                                     ATTR *ap = atr_str(mux_scratch);
                                     if (  ap
