58b6d5adc501785ffe4fa541938b620c8f2d985f
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-09-16 21:49:28 +0000

    Fix gcc 4.1.2 warning about conversion of constant string to (char*). Fix lines that were side-stepping the gcc 4.2.1 warning with a (UTF8*) cast. Fix -Wall errors generated by gcc 4.1.2.

--- mux/src/game.cpp
+++ mux/src/game.cpp
@@ -2218,9 +2218,9 @@ static void info(int fmt, int flags, int ver)
     Log.WriteString(T("\n"));
 }
 
-static UTF8 *standalone_infile = NULL;
-static UTF8 *standalone_outfile = NULL;
-static UTF8 *standalone_basename = NULL;
+static const UTF8 *standalone_infile = NULL;
+static const UTF8 *standalone_outfile = NULL;
+static const UTF8 *standalone_basename = NULL;
 static bool standalone_check = false;
 static bool standalone_load = false;
 static bool standalone_unload = false;
@@ -2457,7 +2457,7 @@ long DebugTotalMemory = 0;
 
 static bool bMinDB = false;
 static bool bSyntaxError = false;
-static UTF8 *conffile = NULL;
+static const UTF8 *conffile = NULL;
 static bool bVersion = false;
 static const UTF8 *pErrorBasename = T("");
 static bool bServerOption = false;
@@ -2489,7 +2489,7 @@ static CLI_OptionEntry OptionTable[NUM_CLI_OPTIONS] =
     { "e", CLI_REQUIRED, CLI_DO_ERRORPATH   }
 };
 
-static void CLI_CallBack(CLI_OptionEntry *p, char *pValue)
+static void CLI_CallBack(CLI_OptionEntry *p, const char *pValue)
 {
     if (p)
     {
