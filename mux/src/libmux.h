8b58f4b91270e07f7f2d52d222465c4e9068d52b
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-09-16 22:35:02 +0000

    Fix gcc 4.1.2 warning about conversion of constant string to (char*). Fix lines that were side-stepping the gcc 4.2.1 warning with a (UTF8*) cast. Fix -Wall errors generated by gcc 4.1.2. Write sending side through Pipe_SendCallPacketAndWait(). Track amount of data in a QUEUE_INFO data structure. Managed to get C39B71F914000000000000002A7BD414020000006D4FB9BA0200000027118B26 across to the stubslave. This is an incorrect frame, but it's progress. Call frame is sent. Channel0_Call() is called in the stubslave. It currently doesn't return the expect proxy CID, but a return frame is given back to netmux, and the creation fails allowing netmux to continue. Don't leave SIGSEGV handler disabled. First working version of out-of-proc. Saw 'ISum::Add(1,1) is 2.' in the logs.

--- mux/src/libmux.h
+++ mux/src/libmux.h
@@ -21,6 +21,12 @@ typedef int MUX_RESULT;
 typedef UINT64 MUX_CID;
 typedef UINT64 MUX_IID;
 
+const UINT8 CallMagic[4]   = { 0xC3, 0x9B, 0x71, 0xF9 };
+const UINT8 ReturnMagic[4] = { 0x35, 0x97, 0x2D, 0xD0 };
+const UINT8 MsgMagic[4]    = { 0xF6, 0x9E, 0x18, 0x36 };
+const UINT8 DiscMagic[4]   = { 0x96, 0x0A, 0xA3, 0x81 };
+const UINT8 EndMagic[4]    = { 0x27, 0x11, 0x8B, 0x26 };
+
 #define MUX_S_OK                 (0)
 #define MUX_S_FALSE              (1)
 #define MUX_E_FAIL              (-1)
@@ -41,7 +47,7 @@ typedef enum
 {
     UseSameProcess  = 1,
     UseMainProcess  = 2,
-    UseSlaveProcess = 3,
+    UseSlaveProcess = 4,
     UseAnyContext   = 7
 } create_context;
 
@@ -151,13 +157,8 @@ typedef struct
 {
     QUEUE_BLOCK *pHead;
     QUEUE_BLOCK *pTail;
+    size_t      nBytes;
 } QUEUE_INFO;
-
-void Pipe_InitializeQueueInfo(QUEUE_INFO *pqi);
-void Pipe_AppendBytes(QUEUE_INFO *pqi, size_t n, const UINT8 *p);
-void Pipe_EmptyQueue(QUEUE_INFO *pqi);
-bool Pipe_GetByte(QUEUE_INFO *pqi, UINT8 ach[0]);
-bool Pipe_GetBytes(QUEUE_INFO *pqi, size_t *pn, UINT8 *pch);
 
 typedef MUX_RESULT FCALL(struct channel_info *pci, QUEUE_INFO *pqi);
 typedef MUX_RESULT FMSG(struct channel_info *pci, QUEUE_INFO *pqi);
@@ -173,7 +174,17 @@ typedef struct channel_info
 } CHANNEL_INFO;
 
 CHANNEL_INFO *Pipe_AllocateChannel(FCALL *pfCall, FMSG *pfMsg, FDISC *pfDisc);
-void Pipe_FreeChannel(CHANNEL_INFO *pci);
+void          Pipe_AppendBytes(QUEUE_INFO *pqi, size_t n, const void *p);
+void          Pipe_AppendQueue(QUEUE_INFO *pqiOut, QUEUE_INFO *pqiIn);
+bool          Pipe_DecodeFrames(UINT32 nReturnChannel, QUEUE_INFO *pqiFrame);
+void          Pipe_EmptyQueue(QUEUE_INFO *pqi);
+void          Pipe_FreeChannel(CHANNEL_INFO *pci);
+bool          Pipe_GetByte(QUEUE_INFO *pqi, UINT8 ach[0]);
+bool          Pipe_GetBytes(QUEUE_INFO *pqi, size_t *pn, void *pch);
+void          Pipe_InitializeChannelZero(FCALL *pfCall0, FMSG *pfMsg0, FDISC *pfDisc0);
+void          Pipe_InitializeQueueInfo(QUEUE_INFO *pqi);
+size_t        Pipe_QueueLength(QUEUE_INFO *pqi);
+MUX_RESULT    Pipe_SendCallPacketAndWait(UINT32 nChannel, QUEUE_INFO *pqi);
 
 // The following is part of what is called 'Custom Marshaling'.
 //
@@ -181,9 +192,9 @@ interface mux_IMarshal : public mux_IUnknown
 {
 public:
     virtual MUX_RESULT GetUnmarshalClass(MUX_IID riid, marshal_context ctx, MUX_CID *pcid) = 0;
-    virtual MUX_RESULT MarshalInterface(size_t *pnBuffer, void **pBuffer, MUX_IID riid, marshal_context ctx) = 0;
-    virtual MUX_RESULT UnmarshalInterface(size_t nBuffer, void *pBuffer, MUX_IID riid, void **ppv) = 0;
-    virtual MUX_RESULT ReleaseMarshalData(char *pBuffer) = 0;
+    virtual MUX_RESULT MarshalInterface(QUEUE_INFO *pqi, MUX_IID riid, marshal_context ctx) = 0;
+    virtual MUX_RESULT UnmarshalInterface(QUEUE_INFO *pqi, MUX_IID riid, void **ppv) = 0;
+    virtual MUX_RESULT ReleaseMarshalData(QUEUE_INFO *pqi) = 0;
     virtual MUX_RESULT DisconnectObject(void) = 0;
 };
 
@@ -227,7 +238,7 @@ typedef void PipePump(void);
 
 // APIs intended only for use by main program (netmux or stubslave).
 //
-extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_InitModuleLibrary(process_context ctx, PipePump *fpPipePump);
+extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_InitModuleLibrary(process_context ctx, PipePump *fpPipePump, QUEUE_INFO *pQueue_In, QUEUE_INFO *pQueue_Out);
 extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_FinalizeModuleLibrary(void);
 
 #ifdef WIN32
@@ -238,7 +249,5 @@ extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_AddModule(const UTF8 aModuleName[],
 extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_RemoveModule(const UTF8 aModuleName[]);
 extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_ModuleInfo(int iModule, MUX_MODULE_INFO *pModuleInfo);
 extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_ModuleMaintenance(void);
-
-extern "C" bool DCL_EXPORT DCL_API mux_ReceiveData(size_t nBuffer, const void *pBuffer);
 
 #endif // LIBMUX_H
--- mux/src/stubslave.cpp
+++ mux/src/stubslave.cpp
@@ -18,270 +18,8 @@ QUEUE_INFO    Queue_In;
 QUEUE_INFO    Queue_Out;
 QUEUE_INFO    Queue_Frame;
 
-typedef enum
-{
-    eUnknown = 0,
-    eCall,
-    eReturn,
-    eMessage,
-    eDisconnect
-} FrameType;
-
-// Decoder
-//
-int           iState = 0;
-FrameType     eType = eUnknown;
-UINT32        nLength = 0;
-UINT32        nChannel = 0;
-size_t        nLengthRemaining = 0;
-
-CHANNEL_INFO *aChannels = NULL;
-int           nChannels = 0;
-int           nChannelsAllocated = 0;
-
-#define CHANNELS_FIRST 100
-
-void Pipe_InitializeChannelZero(FCALL *pfCall0, FMSG *pfMsg0, FDISC *pfDisc0)
-{
-    try
-    {
-        aChannels = new CHANNEL_INFO[CHANNELS_FIRST];
-    }
-    catch (...)
-    {
-        ; // Nothing.
-    }
-
-    if (NULL != aChannels)
-    {
-        nChannelsAllocated      = CHANNELS_FIRST;
-        aChannels[0].nChannel   = 0;
-        aChannels[0].pfCall     = pfCall0;
-        aChannels[0].pfMsg      = pfMsg0;
-        aChannels[0].pfDisc     = pfDisc0;
-        aChannels[0].pInterface = NULL;
-
-        nChannels = 1;
-    }
-}
-
-
-// CallMagic   0xC39B71F9 - 17, 14,  9, 20
-// ReturnMagic 0x35972DD0 -  7, 13,  6, 18
-// MsgMagic    0xF69E1836 - 19, 15,  3,  8
-// DiscMagic   0x960AA381 - 12,  1, 16, 10
-// EndMagic    0x27118B26 -  5,  2, 11,  4
-//
-
-const UINT8 decoder_itt[256] =
-{
-//  0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
-//
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  // 0
-    0,  2,  0,  0,  0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0,  0,  // 1
-    0,  0,  0,  0,  0,  0,  4,  5,  0,  0,  0,  0,  0,  6,  0,  0,  // 2
-    0,  0,  0,  0,  0,  7,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // 3
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // 4
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // 5
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // 6
-    0,  9,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // 7
-
-    0, 10,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11,  0,  0,  0,  0,  // 8
-    0,  0,  0,  0,  0,  0, 12, 13,  0,  0,  0, 14,  0,  0, 15,  0,  // 9
-    0,  0,  0, 16,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // A
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // B
-    0,  0,  0, 17,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // C
-    18, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // D
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  // E
-    0,  0,  0,  0,  0,  0, 19,  0,  0, 20,  0,  0,  0,  0,  0,  0   // F
-};
-
-const UINT8 decoder_stt[23][21] =
-{
-//     0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20
-//
-    {  0,  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0, 20,  0,  0,  0,  0,  1,  0, 17,  0 }, //  0 Start
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2,  0,  0,  0,  0,  0,  0 }, //  1 Call0
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  2 Call1
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4 }, //  3 Call2
-    {  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5 }, //  4 Call3/Return3/Msg3/Disc3
-    {  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6 }, //  5 Length0
-    {  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7 }, //  6 Length1
-    {  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8 }, //  7 Length2
-    { 12, 12, 12, 12, 12,  9, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12 }, //  8 Length3
-    { 12, 12, 10, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12 }, //  9 End0
-    { 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12 }, // 10 End1
-    { 12, 12, 12, 12, 13, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12 }, // 11 End2
-    {  0,  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0, 20,  0,  0,  0,  0,  1,  0, 17,  0 }, // 12 Cleanup
-    {  0,  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0, 20,  0,  0,  0,  0,  1,  0, 17,  0 }, // 13 Accept
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 15,  0,  0,  0,  0,  0,  0,  0 }, // 14 Return0
-    {  0,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 15 Return1
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  0,  0 }, // 16 Return2
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 18,  0,  0,  0,  0,  0 }, // 17 Msg0
-    {  0,  0,  0, 19,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 18 Msg1
-    {  0,  0,  0,  0,  0,  0,  0,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 19 Msg2
-    {  0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 20 Disc0
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0 }, // 21 Disc1
-    {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }  // 22 Disc2
-};
-
-// Decode bytes out of Queue_In to Queue_Frame.
-//
-void Pipe_DecodeFrames(void)
-{
-    UINT8 buffer[QUEUE_BLOCK_SIZE];
-
-    if (8 == iState)
-    {
-        // We must remain in the Length3 state until we have consumed all of the expected data.
-        //
-        while (0 < nLengthRemaining)
-        {
-            size_t nWanted = nLengthRemaining;
-            if (QUEUE_BLOCK_SIZE < nWanted)
-            {
-                nWanted = QUEUE_BLOCK_SIZE;
-            }
-
-            if (  !Pipe_GetBytes(&Queue_In, &nWanted, buffer)
-               || 0 == nWanted)
-            {
-                return;
-            }
-            Pipe_AppendBytes(&Queue_Frame, nWanted, buffer);
-            nLengthRemaining -= nWanted;
-        }
-    }
-
-    UINT8 ch;
-    while (Pipe_GetByte(&Queue_In, &ch))
-    {
-        iState = decoder_stt[iState][decoder_itt[ch]];
-        switch (iState)
-        {
-        case 3: // Call2
-            eType = eCall;
-            break;
-
-        case 16: // Return2
-            eType = eReturn;
-            break;
-
-        case 19: // Msg2
-            eType = eMessage;
-            break;
-
-        case 22: // Disc2
-            eType = eDisconnect;
-            break;
-
-        case 5: // Length0
-            nLength = ((UINT32)ch) << 24;
-            break;
-
-        case 6: // Length1
-            nLength = nLength | (((UINT32)ch) << 16);
-            break;
-
-        case 7: // Length2
-            nLength = nLength | (((UINT32)ch) << 8);
-            break;
-
-        case 8: // Length3
-            nLength = nLength | ((UINT32)ch);
-            nLengthRemaining = nLength;
-              
-            // We've been told how long to expect the packet to be.
-            //
-            while (0 < nLengthRemaining)
-            {
-                size_t nWanted = nLengthRemaining;
-                if (QUEUE_BLOCK_SIZE < nWanted)
-                {
-                    nWanted = QUEUE_BLOCK_SIZE;
-                }
-
-                if (  !Pipe_GetBytes(&Queue_In, &nWanted, buffer)
-                   || 0 == nWanted)
-                {
-                    // We'll leave the state machine and try to pick up again at the same place later.
-                    //
-                    return;
-                }
-                Pipe_AppendBytes(&Queue_Frame, nWanted, buffer);
-                nLengthRemaining -= nWanted;
-            }
-            break;
-
-        case 12: // Cleanup
-
-            // Something went wrong. Re-initialize all the decoding variables.
-            //
-            eType   = eUnknown;
-            nLength = 0;
-            nChannel = 0;
-            Pipe_EmptyQueue(&Queue_Frame);
-            break;
-
-        case 13: // Accept
-            if (4 <= nLength)
-            {
-                UINT8 ach[4];
-                if (  Pipe_GetByte(&Queue_Frame, &ach[0])
-                   && Pipe_GetByte(&Queue_Frame, &ach[1])
-                   && Pipe_GetByte(&Queue_Frame, &ach[2])
-                   && Pipe_GetByte(&Queue_Frame, &ach[3]))
-                {
-                    nChannel = ((UINT32)ach[0]) << 24
-                             | ((UINT32)ach[1]) << 16
-                             | ((UINT32)ach[2]) << 8
-                             | ((UINT32)ch);
-
-                    if (  0 <= nChannel 
-                       && nChannel < nChannels)
-                    {
-                        switch (eType)
-                        {
-                        case eCall:
-                            aChannels[nChannel].pfCall(&aChannels[nChannel], &Queue_Frame);
-
-                            // TODO: The Queue_Frame now contains the return
-                            // paramters. We need to copy these to Queue_Out
-                            // and let them propogate to the other side.
-                            //
-                            break;
-
-                        case eMessage:
-                            aChannels[nChannel].pfMsg(&aChannels[nChannel], &Queue_Frame);
-                            break;
-
-                        case eDisconnect:
-                            aChannels[nChannel].pfDisc(&aChannels[nChannel], &Queue_Frame);
-                            break;
-                        }
-                    }
-                }
-            }
-
-
-            // The packet was too short to contain a channel number, the
-            // channel did not exist, or the call completed successfully.
-            //
-            eType    = eUnknown;
-            nLength  = 0;
-            nChannel = 0;
-            Pipe_EmptyQueue(&Queue_Frame);
-            break;
-        }
-    }
-}
-
 void Stub_ShoveChars(int fdServer)
 {
-    Pipe_InitializeQueueInfo(&Queue_In);
-    Pipe_InitializeQueueInfo(&Queue_Out);
-    Pipe_InitializeQueueInfo(&Queue_Frame);
-
     for (;;)
     {
         UINT8 arg[QUEUE_BLOCK_SIZE];
@@ -302,18 +40,66 @@ void Stub_ShoveChars(int fdServer)
         }
 
         Pipe_AppendBytes(&Queue_In, len, arg);
-        Pipe_DecodeFrames();
+        Pipe_DecodeFrames(0xFFFFFFFFUL, &Queue_Frame);
+        
+        size_t nWanted = sizeof(arg);
+        while (  Pipe_GetBytes(&Queue_Out, &nWanted, arg)
+              && 0 < nWanted)
+        {
+            write(1, arg, nWanted);
+        }
     }
+}
+
+void Stub_PipePump(void)
+{
 }
 
 MUX_RESULT Channel0_Call(CHANNEL_INFO *pci, QUEUE_INFO *pqi)
 {
-    return MUX_E_NOTIMPLEMENTED;
+    struct CF
+    {
+        MUX_CID cid;
+        MUX_IID iid;
+    } CallFrame;
+
+    MUX_RESULT mr = MUX_S_OK;
+
+    size_t nWanted = sizeof(CallFrame);
+    if (  Pipe_GetBytes(pqi, &nWanted, &CallFrame)
+       && nWanted == sizeof(CallFrame))
+    {
+        mux_IMarshal *pIMarshal = NULL;
+        mr = mux_CreateInstance(CallFrame.cid, NULL, UseSameProcess, mux_IID_IMarshal, (void **)&pIMarshal);
+        if (MUX_SUCCEEDED(mr))
+        {
+            MUX_CID cidProxy = 0;
+            mr = pIMarshal->GetUnmarshalClass(CallFrame.iid, CrossProcess, &cidProxy);
+            if (MUX_SUCCEEDED(mr))
+            {
+                Pipe_AppendBytes(pqi, sizeof(cidProxy), &cidProxy);
+                mr = pIMarshal->MarshalInterface(pqi, CallFrame.iid, CrossProcess);
+            }
+            pIMarshal->Release();
+        }
+    }
+    else
+    {
+       Pipe_EmptyQueue(pqi);
+       mr = MUX_E_INVALIDARG;
+    }
+    return mr;
 }
 
 int main(int argc, char *argv[])
 {
-    mux_InitModuleLibrary(IsSlaveProcess, NULL);
+    Pipe_InitializeQueueInfo(&Queue_In);
+    Pipe_InitializeQueueInfo(&Queue_Out);
+    Pipe_InitializeQueueInfo(&Queue_Frame);
+
+    mux_InitModuleLibrary(IsSlaveProcess, Stub_PipePump, &Queue_In, &Queue_Out);
+    mux_AddModule(T("sum"), T("./bin/sum.so"));
+    
     Pipe_InitializeChannelZero(Channel0_Call, NULL, NULL);
     Stub_ShoveChars(0);
     mux_FinalizeModuleLibrary();
