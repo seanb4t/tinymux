fd0f66bcefc455a0ddb59cb93877588ced3b1e82
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-09-20 15:50:52 +0000

    Move Channel0_Call() into libmux.cpp because the same one is used by netmux and stubslave. Refactor Stub_ShoveChars() and Stub_PipePump() in stubslave. CHANNEL_INVALID is UINT32 not UINT64. Bring magic constant into libmux.cpp. Update comments. Fix conversion of ANSI sequences containing semi-colons.

--- mux/src/stubslave.cpp
+++ mux/src/stubslave.cpp
@@ -16,92 +16,48 @@
 
 QUEUE_INFO    Queue_In;
 QUEUE_INFO    Queue_Out;
-QUEUE_INFO    Queue_Frame;
-
-void Stub_ShoveChars(int fdServer)
-{
-    for (;;)
-    {
-        UINT8 arg[QUEUE_BLOCK_SIZE];
-        int len = read(fdServer, arg, sizeof(arg));
-        if (len == 0)
-        {
-            break;
-        }
-
-        if (len < 0)
-        {
-            if (EINTR == errno)
-            {
-                errno = 0;
-                continue;
-            }
-            break;
-        }
-
-        Pipe_AppendBytes(&Queue_In, len, arg);
-        Pipe_DecodeFrames(0xFFFFFFFFUL, &Queue_Frame);
-        
-        size_t nWanted = sizeof(arg);
-        while (  Pipe_GetBytes(&Queue_Out, &nWanted, arg)
-              && 0 < nWanted)
-        {
-            write(1, arg, nWanted);
-        }
-    }
-}
 
 void Stub_PipePump(void)
 {
+    static UINT8 arg[QUEUE_BLOCK_SIZE];
+    size_t nWanted = sizeof(arg);
+    while (  Pipe_GetBytes(&Queue_Out, &nWanted, arg)
+          && 0 < nWanted)
+    {
+        write(1, arg, nWanted);
+        nWanted = sizeof(arg);
+    }
+
+    int len = read(0, arg, sizeof(arg));
+    if (0 < len)
+    {
+        Pipe_AppendBytes(&Queue_In, len, arg);
+    }
 }
 
-MUX_RESULT Channel0_Call(CHANNEL_INFO *pci, QUEUE_INFO *pqi)
+bool bStubSlaveShutdown = false;
+
+void Stub_ShoveChars(void)
 {
-    struct CF
-    {
-        MUX_CID cid;
-        MUX_IID iid;
-    } CallFrame;
+    QUEUE_INFO Queue_Frame;
+    Pipe_InitializeQueueInfo(&Queue_Frame);
 
-    MUX_RESULT mr = MUX_S_OK;
-
-    size_t nWanted = sizeof(CallFrame);
-    if (  Pipe_GetBytes(pqi, &nWanted, &CallFrame)
-       && nWanted == sizeof(CallFrame))
+    while (!bStubSlaveShutdown)
     {
-        mux_IMarshal *pIMarshal = NULL;
-        mr = mux_CreateInstance(CallFrame.cid, NULL, UseSameProcess, mux_IID_IMarshal, (void **)&pIMarshal);
-        if (MUX_SUCCEEDED(mr))
-        {
-            MUX_CID cidProxy = 0;
-            mr = pIMarshal->GetUnmarshalClass(CallFrame.iid, CrossProcess, &cidProxy);
-            if (MUX_SUCCEEDED(mr))
-            {
-                Pipe_AppendBytes(pqi, sizeof(cidProxy), &cidProxy);
-                mr = pIMarshal->MarshalInterface(pqi, CallFrame.iid, CrossProcess);
-            }
-            pIMarshal->Release();
-        }
+        Stub_PipePump();
+        Pipe_DecodeFrames(CHANNEL_INVALID, &Queue_Frame);
     }
-    else
-    {
-       Pipe_EmptyQueue(pqi);
-       mr = MUX_E_INVALIDARG;
-    }
-    return mr;
 }
 
 int main(int argc, char *argv[])
 {
     Pipe_InitializeQueueInfo(&Queue_In);
     Pipe_InitializeQueueInfo(&Queue_Out);
-    Pipe_InitializeQueueInfo(&Queue_Frame);
 
     mux_InitModuleLibrary(IsSlaveProcess, Stub_PipePump, &Queue_In, &Queue_Out);
     mux_AddModule(T("sum"), T("./bin/sum.so"));
-    
-    Pipe_InitializeChannelZero(Channel0_Call, NULL, NULL);
-    Stub_ShoveChars(0);
+
+    Stub_ShoveChars();
     mux_FinalizeModuleLibrary();
     return 0;
 }
