26a6e331c322c8301ddff72ac94febcae2b5e1b7
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-10-28 03:35:43 +0000

    Describe recent changes. Fix VS2005 warnings. Fix more VS2005 warnings. Empty is fine. NULL is not. Fix dereference of NULL warning. Fix more VS2005 warnings. r1355 moved the loop variable allowed an endless loop (Issue 403). Document fix for foreach() hang. Document chanobj() in CHANGES. fun_foreach now silently defaults to classic behavior if the third or fourth argument is empty. Codereview foreach() workaround. Fix buffer overflow with %=<> substitution (Issue 406). Update @comon help topic (Issue 404). Bump version number to 2.7.0.12.

--- mux/src/funceval.cpp
+++ mux/src/funceval.cpp
@@ -2959,6 +2959,9 @@ FUNCTION(fun_lastcreate)
 {
     UNUSED_PARAMETER(caller);
     UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(eval);
+    UNUSED_PARAMETER(ncargs);
+    UNUSED_PARAMETER(cargs);
 
     // Determine the target by name, or use the executor if no name is given.
     //
@@ -3315,7 +3318,9 @@ FUNCTION(fun_foreach)
     size_t nStr = sStr->length();
     LBUF_OFFSET i = 0, nBytes = 0;
 
-    if (nfargs == 4)
+    if (  4 == nfargs
+       && '\0' != fargs[2]
+       && '\0' != fargs[3])
     {
         bool flag = false;
         UTF8 prev = '\0';
@@ -3325,6 +3330,7 @@ FUNCTION(fun_foreach)
               && !MuxAlarm.bAlarmed)
         {
             nBytes = sStr->export_Char_UTF8(i, cbuf);
+            i = i + nBytes;
 
             if (flag)
             {
@@ -3355,7 +3361,6 @@ FUNCTION(fun_foreach)
             mux_exec(atext, LBUF_SIZE-1, buff, bufc, thing, executor, enactor,
                 AttrTrace(aflags, EV_STRIP_CURLY|EV_FCHECK|EV_EVAL), &bp, 1);
             prev = cbuf[0];
-            i = i + nBytes;
         }
     }
     else
