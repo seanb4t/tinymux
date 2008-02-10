fa9f24bec1d17de3b039faf54513d0da37780979
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2008-02-10 16:57:32 +0000

    @email and sql no longer require --enable-firanmux. SQL can be turned on with --enable-inlinesql or --enable-stubslave.

--- mux/src/conf.cpp
+++ mux/src/conf.cpp
@@ -110,17 +110,15 @@ void cf_init(void)
     mux_strncpy(mudconf.guests_channel, T("Guests"), 31);
     mux_strncpy(mudconf.guests_channel_alias, T("g"), 31);
     mux_strncpy(mudconf.pueblo_msg, T("</xch_mudtext><img xch_mode=html>"), GBUF_SIZE-1);
-
+#if defined(FIRANMUX)
+    mux_strncpy(mudconf.immobile_msg, T("You have been set immobile."), sizeof(mudconf.immobile_msg)-1);
+#endif // FIRANMUX
 #if defined(INLINESQL) || defined(TINYMUX_MODULES)
     mudconf.sql_server[0]   = '\0';
     mudconf.sql_user[0]     = '\0';
     mudconf.sql_password[0] = '\0';
     mudconf.sql_database[0] = '\0';
 #endif // INLINESQL || TINYMUX_MODULES
-
-#if defined(FIRANMUX)
-    mux_strncpy(mudconf.immobile_msg, T("You have been set immobile."), sizeof(mudconf.immobile_msg)-1);
-#endif // FIRANMUX
 
     mudconf.mail_server[0]  = '\0';
     mudconf.mail_ehlo[0]    = '\0';
