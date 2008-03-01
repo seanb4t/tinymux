c9aad26d582cefaf0e9e75e75579a1ff3429f268
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2008-03-01 15:52:31 +0000

    Don't QueryInterface on every call. Add more code for the Standard Marshaller. Move logging options to modules.h for mux_ILog. Update VS project dependencies. Fix several problems with @reference (Issue 491).

--- mux/src/libmux.cpp
+++ mux/src/libmux.cpp
@@ -1313,27 +1313,29 @@ extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_FinalizeModuleLibrary(void)
 
 static MUX_RESULT CStd_Call(CHANNEL_INFO *pci, QUEUE_INFO *pqi)
 {
+    mux_IRpcStubBuffer *pIRpcStubBuffer = static_cast<mux_IRpcStubBuffer *>(pci->pInterface);
+    if (NULL == pIRpcStubBuffer)
+    {
+        return MUX_E_NOINTERFACE;
+    }
+
+    //return pIRpcStubBuffer->Invoke();
     return MUX_E_NOTIMPLEMENTED;
 }
 
 static MUX_RESULT CStd_Disconnect(CHANNEL_INFO *pci, QUEUE_INFO *pqi)
 {
-    mux_IUnknown *pIUnknown = static_cast<mux_IUnknown *>(pci->pInterface);
-    if (NULL == pIUnknown)
+    mux_IRpcStubBuffer *pIRpcStubBuffer = static_cast<mux_IRpcStubBuffer *>(pci->pInterface);
+    if (NULL == pIRpcStubBuffer)
     {
         return MUX_E_NOINTERFACE;
     }
 
-    mux_IRpcStubBuffer *pIRpcStubBuffer = NULL;
-    MUX_RESULT mr = pIUnknown->QueryInterface(mux_IID_IRpcStubBuffer, (void **)&pIRpcStubBuffer);
-    if (MUX_SUCCEEDED(mr))
-    {
-        pIRpcStubBuffer->Disconnect();
-        pIRpcStubBuffer->Release();
-        pci->pInterface = NULL;
-        Pipe_FreeChannel(pci);
-    }
-    return mr;
+    pIRpcStubBuffer->Disconnect();
+    pIRpcStubBuffer->Release();
+    pci->pInterface = NULL;
+    Pipe_FreeChannel(pci);
+    return MUX_S_OK;
 }
 
 #endif // ENABLE_STD_MARSHALER
@@ -1478,11 +1480,65 @@ extern "C" MUX_RESULT DCL_EXPORT DCL_API mux_UnmarshalInterface(QUEUE_INFO *pqi,
                     mr = mux_CreateInstance(cidProxyStub, NULL, UseSameProcess, mux_IID_IPSFactoryBuffer, (void **)&pIPSFactoryBuffer);
                     if (MUX_SUCCEEDED(mr))
                     {
-                        // TODO: Call CreateProxy, construct a private
-                        // CRpcChannelBuffer using nChannel, and
-                        // associate the two.
-                        //
-                        mr = MUX_E_NOTIMPLEMENTED;
+                        if (NULL == g_ProxyManager)
+                        {
+                            g_ProxyManager = CreateProxyManger();
+                        }
+
+                        if (NULL != g_ProxyManager)
+                        {
+                            mux_IUnknown *pTemp = NULL;
+                            mux_IRpcProxyBuffer *pRpcProxyBuffer = NULL;
+                            mr = pIPSFactoryBuffer->CreateProxy(g_pProxyManager, riid, &pRpcProxyBuffer, (void **)&pTemp);
+                            if (MUX_SUCCEEDED(mr))
+                            {
+                                mux_IRpcChannelBuffer *pRpcChannelBuffer = NULL;
+                                try
+                                {
+                                    pRpcChannelBuffer = new CRpcChannelBuffer(nChannel);
+                                }
+                                catch (...)
+                                {
+                                    ; // Nothing.
+                                }
+
+                                if (NULL != pRpcChannelBuffer}
+                                {
+                                    mr = pRpcProxyBuffer->Connect(pRpcChannelBuffer);
+                                    if (MUX_SUCCEEDED(mr))
+                                    {
+                                        mr = MUX_E_NOTIMPLEMENTED;
+                                    }
+                                    else
+                                    {
+                                        mr = MUX_E_CLASSNOTAVAILABLE;
+                                    }
+                                }
+                                else
+                                {
+                                    mr = MUX_E_OUTOFMEMORY;
+                                }
+
+                                if (MUX_FAILED(mr))
+                                {
+                                    pRpcProxyBuffer->Release();
+                                    pTemp->Release();
+                                }
+                            }
+                            else
+                            {
+                                mr = MUX_E_CLASSNOTAVAILABLE;
+                            }
+                        }
+                        else
+                        {
+                            mr = MUX_E_CLASSNOTAVAILABLE;
+                        }
+                        pIPSFactoryBuffer->Release();
+                    }
+                    else
+                    {
+                        mr = MUX_E_CLASSNOTAVAILABLE;
                     }
                 }
                 else
