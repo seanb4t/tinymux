1a5a7c03c39bd0d73f97fa8b4a9e6def40f91f7c
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2008-04-13 05:30:36 +0000

    Generate separate copy/paste for stringutil.cpp and stringutil.h to make it less manual.

--- mux/src/stringutil.h
+++ mux/src/stringutil.h
@@ -104,6 +104,16 @@ inline size_t TrimPartialSequence(size_t n, __in_ecount(n) const UTF8 *p)
 
 #define utf8_NextCodePoint(x)      (x + utf8_FirstByte[(unsigned char)*x])
 
+typedef struct
+{
+    size_t n_bytes;
+    size_t n_points;
+    const UTF8 *p;
+} string_desc;
+
+// Beginning of Unicode Table Definitions.
+//
+
 // utf/cl_Printable.txt
 //
 // 100312 included, 1013800 excluded, 0 errors.
@@ -114,6 +124,146 @@ inline size_t TrimPartialSequence(size_t n, __in_ecount(n) const UTF8 *p)
 extern const unsigned char cl_print_itt[256];
 extern const unsigned char cl_print_stt[198][95];
 
+// utf/cl_AttrNameInitial.txt
+//
+// 177 included, 1113935 excluded, 0 errors.
+// 6 states, 14 columns, 340 bytes
+//
+#define CL_ATTRNAMEINITIAL_START_STATE (0)
+#define CL_ATTRNAMEINITIAL_ACCEPTING_STATES_START (6)
+extern const unsigned char cl_attrnameinitial_itt[256];
+extern const unsigned char cl_attrnameinitial_stt[6][14];
+
+// utf/cl_AttrName.txt
+//
+// 203 included, 1113909 excluded, 0 errors.
+// 6 states, 14 columns, 340 bytes
+//
+#define CL_ATTRNAME_START_STATE (0)
+#define CL_ATTRNAME_ACCEPTING_STATES_START (6)
+extern const unsigned char cl_attrname_itt[256];
+extern const unsigned char cl_attrname_stt[6][14];
+
+// utf/cl_ObjectName.txt
+//
+// 257 included, 1113855 excluded, 0 errors.
+// 8 states, 23 columns, 440 bytes
+//
+#define CL_OBJECTNAME_START_STATE (0)
+#define CL_OBJECTNAME_ACCEPTING_STATES_START (8)
+extern const unsigned char cl_objectname_itt[256];
+extern const unsigned char cl_objectname_stt[8][23];
+
+// utf/cl_PlayerName.txt
+//
+// 190 included, 1113922 excluded, 0 errors.
+// 6 states, 14 columns, 340 bytes
+//
+#define CL_PLAYERNAME_START_STATE (0)
+#define CL_PLAYERNAME_ACCEPTING_STATES_START (6)
+extern const unsigned char cl_playername_itt[256];
+extern const unsigned char cl_playername_stt[6][14];
+
+// utf/cl_8859_1.txt
+//
+// 191 included, 1113921 excluded, 0 errors.
+// 3 states, 6 columns, 274 bytes
+//
+#define CL_8859_1_START_STATE (0)
+#define CL_8859_1_ACCEPTING_STATES_START (3)
+extern const unsigned char cl_8859_1_itt[256];
+extern const unsigned char cl_8859_1_stt[3][6];
+
+// utf/cl_8859_2.txt
+//
+// 191 included, 1113921 excluded, 0 errors.
+// 6 states, 21 columns, 382 bytes
+//
+#define CL_8859_2_START_STATE (0)
+#define CL_8859_2_ACCEPTING_STATES_START (6)
+extern const unsigned char cl_8859_2_itt[256];
+extern const unsigned char cl_8859_2_stt[6][21];
+
+// utf/tr_utf8_latin1.txt
+//
+// 2461 code points.
+// 97 states, 193 columns, 37698 bytes
+//
+#define TR_LATIN1_START_STATE (0)
+#define TR_LATIN1_ACCEPTING_STATES_START (97)
+extern const unsigned char tr_latin1_itt[256];
+extern const unsigned short tr_latin1_stt[97][193];
+
+// utf/tr_utf8_ascii.txt
+//
+// 2424 code points.
+// 95 states, 193 columns, 18591 bytes
+//
+#define TR_ASCII_START_STATE (0)
+#define TR_ASCII_ACCEPTING_STATES_START (95)
+extern const unsigned char tr_ascii_itt[256];
+extern const unsigned char tr_ascii_stt[95][193];
+
+// utf/tr_tolower.txt
+//
+// 1023 code points.
+// 46 states, 86 columns, 4212 bytes
+//
+#define TR_TOLOWER_START_STATE (0)
+#define TR_TOLOWER_ACCEPTING_STATES_START (46)
+extern const unsigned char tr_tolower_itt[256];
+extern const unsigned char tr_tolower_stt[46][86];
+
+#define TR_TOLOWER_DEFAULT (0)
+#define TR_TOLOWER_LITERAL_START (1)
+#define TR_TOLOWER_XOR_START (13)
+extern const string_desc tr_tolower_ott[97];
+
+// utf/tr_toupper.txt
+//
+// 1030 code points.
+// 48 states, 90 columns, 4576 bytes
+//
+#define TR_TOUPPER_START_STATE (0)
+#define TR_TOUPPER_ACCEPTING_STATES_START (48)
+extern const unsigned char tr_toupper_itt[256];
+extern const unsigned char tr_toupper_stt[48][90];
+
+#define TR_TOUPPER_DEFAULT (0)
+#define TR_TOUPPER_LITERAL_START (1)
+#define TR_TOUPPER_XOR_START (11)
+extern const string_desc tr_toupper_ott[102];
+
+// utf/tr_totitle.txt
+//
+// 1034 code points.
+// 48 states, 90 columns, 4576 bytes
+//
+#define TR_TOTITLE_START_STATE (0)
+#define TR_TOTITLE_ACCEPTING_STATES_START (48)
+extern const unsigned char tr_totitle_itt[256];
+extern const unsigned char tr_totitle_stt[48][90];
+
+#define TR_TOTITLE_DEFAULT (0)
+#define TR_TOTITLE_LITERAL_START (1)
+#define TR_TOTITLE_XOR_START (11)
+extern const string_desc tr_totitle_ott[100];
+
+// utf/tr_Color.txt
+//
+// 517 code points.
+// 5 states, 13 columns, 321 bytes
+//
+#define TR_COLOR_START_STATE (0)
+#define TR_COLOR_ACCEPTING_STATES_START (5)
+extern const unsigned char tr_color_itt[256];
+extern const unsigned char tr_color_stt[5][13];
+
+//
+// End of Unicode Table Definitions.
+
+// utf/cl_Printable.txt
+//
 inline bool mux_isprint(__in const unsigned char *p)
 {
     int iState = CL_PRINT_START_STATE;
@@ -127,14 +277,6 @@ inline bool mux_isprint(__in const unsigned char *p)
 
 // utf/cl_AttrNameInitial.txt
 //
-// 177 included, 1113935 excluded, 0 errors.
-// 6 states, 14 columns, 340 bytes
-//
-#define CL_ATTRNAMEINITIAL_START_STATE (0)
-#define CL_ATTRNAMEINITIAL_ACCEPTING_STATES_START (6)
-extern const unsigned char cl_attrnameinitial_itt[256];
-extern const unsigned char cl_attrnameinitial_stt[6][14];
-
 inline bool mux_isattrnameinitial(__in const unsigned char *p)
 {
     int iState = CL_ATTRNAMEINITIAL_START_STATE;
@@ -148,14 +290,6 @@ inline bool mux_isattrnameinitial(__in const unsigned char *p)
 
 // utf/cl_AttrName.txt
 //
-// 203 included, 1113909 excluded, 0 errors.
-// 6 states, 14 columns, 340 bytes
-//
-#define CL_ATTRNAME_START_STATE (0)
-#define CL_ATTRNAME_ACCEPTING_STATES_START (6)
-extern const unsigned char cl_attrname_itt[256];
-extern const unsigned char cl_attrname_stt[6][14];
-
 inline bool mux_isattrname(__in const unsigned char *p)
 {
     int iState = CL_ATTRNAME_START_STATE;
@@ -169,14 +303,6 @@ inline bool mux_isattrname(__in const unsigned char *p)
 
 // utf/cl_Objectname.txt
 //
-// 257 included, 1113855 excluded, 0 errors.
-// 8 states, 23 columns, 440 bytes
-//
-#define CL_OBJECTNAME_START_STATE (0)
-#define CL_OBJECTNAME_ACCEPTING_STATES_START (8)
-extern const unsigned char cl_objectname_itt[256];
-extern const unsigned char cl_objectname_stt[8][23];
-
 inline bool mux_isobjectname(__in const unsigned char *p)
 {
     int iState = CL_OBJECTNAME_START_STATE;
@@ -190,14 +316,6 @@ inline bool mux_isobjectname(__in const unsigned char *p)
 
 // utf/cl_PlayerName.txt
 //
-// 190 included, 1113922 excluded, 0 errors.
-// 6 states, 14 columns, 340 bytes
-//
-#define CL_PLAYERNAME_START_STATE (0)
-#define CL_PLAYERNAME_ACCEPTING_STATES_START (6)
-extern const unsigned char cl_playername_itt[256];
-extern const unsigned char cl_playername_stt[6][14];
-
 inline bool mux_isplayername(__in const unsigned char *p)
 {
     int iState = CL_PLAYERNAME_START_STATE;
@@ -211,14 +329,6 @@ inline bool mux_isplayername(__in const unsigned char *p)
 
 // utf/cl_8859_1.txt
 //
-// 191 included, 1113921 excluded, 0 errors.
-// 3 states, 6 columns, 274 bytes
-//
-#define CL_8859_1_START_STATE (0)
-#define CL_8859_1_ACCEPTING_STATES_START (3)
-extern const unsigned char cl_8859_1_itt[256];
-extern const unsigned char cl_8859_1_stt[3][6];
-
 inline bool mux_is8859_1(__in const unsigned char *p)
 {
     int iState = CL_8859_1_START_STATE;
@@ -232,14 +342,6 @@ inline bool mux_is8859_1(__in const unsigned char *p)
 
 // utf/cl_8859_2.txt
 //
-// 191 included, 1113921 excluded, 0 errors.
-// 6 states, 21 columns, 382 bytes
-//
-#define CL_8859_2_START_STATE (0)
-#define CL_8859_2_ACCEPTING_STATES_START (6)
-extern const unsigned char cl_8859_2_itt[256];
-extern const unsigned char cl_8859_2_stt[6][21];
-
 inline bool mux_is8859_2(__in const unsigned char *p)
 {
     int iState = CL_8859_2_START_STATE;
@@ -253,47 +355,14 @@ inline bool mux_is8859_2(__in const unsigned char *p)
 
 // utf/tr_utf8_latin1.txt
 //
-// 2461 code points.
-// 97 states, 193 columns, 37698 bytes
-//
-#define TR_LATIN1_START_STATE (0)
-#define TR_LATIN1_ACCEPTING_STATES_START (97)
-extern const unsigned char tr_latin1_itt[256];
-extern const unsigned short tr_latin1_stt[97][193];
 const char *ConvertToLatin(__in const UTF8 *pString);
 
 // utf/tr_utf8_ascii.txt
 //
-// 2424 code points.
-// 95 states, 193 columns, 18591 bytes
-//
-#define TR_ASCII_START_STATE (0)
-#define TR_ASCII_ACCEPTING_STATES_START (95)
-extern const unsigned char tr_ascii_itt[256];
-extern const unsigned char tr_ascii_stt[95][193];
 const char *ConvertToAscii(__in const UTF8 *pString);
-
-typedef struct
-{
-    size_t n_bytes;
-    size_t n_points;
-    const UTF8 *p;
-} string_desc;
 
 // utf/tr_tolower.txt
 //
-// 1023 code points.
-// 46 states, 86 columns, 4212 bytes
-//
-#define TR_TOLOWER_START_STATE (0)
-#define TR_TOLOWER_ACCEPTING_STATES_START (46)
-#define TR_TOLOWER_DEFAULT (0)
-#define TR_TOLOWER_LITERAL_START (1)
-#define TR_TOLOWER_XOR_START (13)
-extern const unsigned char tr_tolower_itt[256];
-extern const unsigned char tr_tolower_stt[46][86];
-extern const string_desc tr_tolower_ott[97];
-
 inline const string_desc *mux_tolower(__in const unsigned char *p, bool &bXor)
 {
     int iState = TR_TOLOWER_START_STATE;
@@ -317,18 +386,6 @@ inline const string_desc *mux_tolower(__in const unsigned char *p, bool &bXor)
 
 // utf/tr_toupper.txt
 //
-// 1030 code points.
-// 48 states, 90 columns, 4576 bytes
-//
-#define TR_TOUPPER_START_STATE (0)
-#define TR_TOUPPER_ACCEPTING_STATES_START (48)
-#define TR_TOUPPER_DEFAULT (0)
-#define TR_TOUPPER_LITERAL_START (1)
-#define TR_TOUPPER_XOR_START (11)
-extern const unsigned char tr_toupper_itt[256];
-extern const unsigned char tr_toupper_stt[48][90];
-extern const string_desc tr_toupper_ott[102];
-
 inline const string_desc *mux_toupper(__in const unsigned char *p, bool &bXor)
 {
     int iState = TR_TOUPPER_START_STATE;
@@ -352,18 +409,6 @@ inline const string_desc *mux_toupper(__in const unsigned char *p, bool &bXor)
 
 // utf/tr_totitle.txt
 //
-// 1034 code points.
-// 48 states, 90 columns, 4576 bytes
-//
-#define TR_TOTITLE_START_STATE (0)
-#define TR_TOTITLE_ACCEPTING_STATES_START (48)
-#define TR_TOTITLE_DEFAULT (0)
-#define TR_TOTITLE_LITERAL_START (1)
-#define TR_TOTITLE_XOR_START (11)
-extern const unsigned char tr_totitle_itt[256];
-extern const unsigned char tr_totitle_stt[48][90];
-extern const string_desc tr_totitle_ott[100];
-
 inline const string_desc *mux_totitle(__in const unsigned char *p, bool &bXor)
 {
     int iState = TR_TOTITLE_START_STATE;
@@ -387,14 +432,6 @@ inline const string_desc *mux_totitle(__in const unsigned char *p, bool &bXor)
 
 // utf/tr_Color.txt
 //
-// 517 code points.
-// 5 states, 13 columns, 321 bytes
-//
-#define TR_COLOR_START_STATE (0)
-#define TR_COLOR_ACCEPTING_STATES_START (5)
-extern const unsigned char tr_color_itt[256];
-extern const unsigned char tr_color_stt[5][13];
-
 inline int mux_color(__in const unsigned char *p)
 {
     int iState = TR_COLOR_START_STATE;
