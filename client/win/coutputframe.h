7b1947dc5e8328b7c33d9b550682f72b3d720467
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2008-05-14 04:25:58 +0000

    Create Input Window. Add RichEdit control to the Input and Output Windows. When enter is touched, move text from input window to output window. Append and add scroll bars. Automatically scroll output window down. Handle large pastes. Postpone last CRLF to output window Force focus to Input Window's control. Git ride of background brush to avoid flash. Raise text limit to 20MB. The examples for WIDTH() and HEIGHT() are for 'say' instead of 'think'.

--- client/win/coutputframe.h
+++ client/win/coutputframe.h
@@ -5,18 +5,19 @@ class COutputFrame : public CWindow
 {
 public:
     static LRESULT CALLBACK OutputWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+    static DWORD CALLBACK EditStreamCallback(DWORD dwCookie, LPBYTE pbBuff, LONG cb, LONG *pcb);
+
     COutputFrame();
     bool Create(CWindow *pParentWindow, int x, int y, int cx, int cy);
+    void AppendText(size_t nBuffer, WCHAR *pBuffer);
     ~COutputFrame();
 
     // Handlers.
     //
     LRESULT OnCreate(CREATESTRUCT *pcs);
-    LRESULT OnPaint(void);
+    void OnSize(UINT nType, int cx, int cy);
 
-    // Document stuff.
-    //
-    WCHAR        m_szHello[MAX_LOADSTRING];
+    HWND    m_hwndRichEdit;
 };
 
 #endif // COUTPUTFRAME_H
