154e034b8bce2367763adbc3bb671d801578befa
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2008-05-20 14:50:35 +0000

    Create Input Window. Add RichEdit control to the Input and Output Windows. When enter is touched, move text from input window to output window. Append and add scroll bars. Automatically scroll output window down. Handle large pastes. Postpone last CRLF to output window Force focus to Input Window's control. Git rid of background brush to avoid flash. Raise text limit to 20MB. The examples for WIDTH() and HEIGHT() are for 'say' instead of 'think'. Fix formating. Fix whisper to cause error messages to preceed other notifications. Per Issue 456.

--- client/win/coutputframe.cpp
+++ client/win/coutputframe.cpp
@@ -35,26 +35,104 @@ LRESULT COutputFrame::OnCreate(CREATESTRUCT *pcs)
     {
         m_pParentWindow = pcp->pParentWindow;
     }
-    g_theApp.LoadString(IDS_HELLO, m_szHello, MAX_LOADSTRING);
+
+    if (g_theApp.m_bMsftEdit)
+    {
+        // Rich Edit 4.1 is available on Windows XP SP1 and later.
+        //
+        m_hwndRichEdit = ::CreateWindowEx(0L, MSFTEDIT_CLASS, NULL,
+            ES_MULTILINE | ES_READONLY | ES_AUTOVSCROLL | WS_CHILD | WS_BORDER | WS_CLIPCHILDREN | WS_VISIBLE | WS_VSCROLL,
+            pcs->x, pcs->y, pcs->cx, pcs->cy,
+            m_hwnd, NULL, g_theApp.m_hRichEdit, NULL);
+    }
+    else
+    {
+        // Rich Edit 2.0 (98/NT4) or 3.0 (Me/2K/XP) control.
+        //
+        m_hwndRichEdit = ::CreateWindowEx(0L, RICHEDIT_CLASS, NULL,
+            ES_MULTILINE | ES_READONLY | ES_AUTOVSCROLL | WS_CHILD | WS_BORDER | WS_CLIPCHILDREN | WS_VISIBLE | WS_VSCROLL,
+            pcs->x, pcs->y, pcs->cx, pcs->cy,
+            m_hwnd, NULL, g_theApp.m_hRichEdit, NULL);
+    }
+    if (NULL == m_hwndRichEdit)
+    {
+        return 1;
+    }
+
+    CHARFORMAT2 cf2;
+    memset(&cf2, 0, sizeof(cf2));
+    cf2.cbSize = sizeof(cf2);
+    cf2.dwMask = CFM_FACE | CFM_COLOR | CFM_BACKCOLOR | CFM_SIZE;
+    cf2.crTextColor = RGB(255,128,0);
+    cf2.crBackColor = RGB(60,60,60);
+    cf2.yHeight = 20*14;
+    memcpy(cf2.szFaceName, L"Courier New", sizeof(L"Curier New"));
+    LRESULT lRes = ::SendMessage(m_hwndRichEdit, EM_SETBKGNDCOLOR, 0, (LPARAM)cf2.crBackColor);
+    if (g_theApp.m_bMsftEdit)
+    {
+        lRes = ::SendMessage(m_hwndRichEdit, EM_SETCHARFORMAT, SCF_ALL, (LPARAM) &cf2);
+    }
+    else
+    {
+        lRes = ::SendMessage(m_hwndRichEdit, EM_SETCHARFORMAT, SCF_DEFAULT, (LPARAM) &cf2);
+    }
     return 0;
 }
 
-LRESULT COutputFrame::OnPaint(void)
+void COutputFrame::OnSize(UINT nType, int cx, int cy)
 {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(m_hwnd, &ps);
-    RECT rt;
-    GetClientRect(m_hwnd, &rt);
-    FillRect(hdc, &rt, g_theApp.m_brushBlack);
-    DrawText(hdc, m_szHello, wcslen(m_szHello), &rt, DT_CENTER);
-    EndPaint(m_hwnd, &ps);
+    ::MoveWindow(m_hwndRichEdit, 0, 0, cx, cy, true);
+}
+
+typedef struct
+{
+    LONG   cb;
+    LPBYTE pbBuff;
+} STREAMFRAGMENT;
+
+DWORD CALLBACK COutputFrame::EditStreamCallback(DWORD dwCookie, LPBYTE pbBuff, LONG cb, LONG *pcb)
+{
+    STREAMFRAGMENT *psf = (STREAMFRAGMENT *)dwCookie;
+    LONG cbMove = cb;
+    if (psf->cb < cbMove)
+    {
+        cbMove = psf->cb;
+    }
+    memcpy(pbBuff, psf->pbBuff, cbMove);
+    *pcb = cbMove;
+    psf->cb -= cbMove;
+    psf->pbBuff += cbMove;
     return 0;
+}
+
+void COutputFrame::AppendText(size_t nBuffer, WCHAR *pBuffer)
+{
+    // Position to the end.
+    //
+    CHARRANGE cr;
+    cr.cpMin = -1; // nLength;
+    cr.cpMax = -1; // nLength;
+    (void)::SendMessage(m_hwndRichEdit, EM_EXSETSEL, 0, (LPARAM)&cr);
+
+    STREAMFRAGMENT sf;
+    sf.cb = nBuffer*sizeof(WCHAR);
+    sf.pbBuff = (BYTE *)pBuffer;
+
+    EDITSTREAM es;
+    es.dwCookie = (DWORD)&sf;
+    es.dwError  = ERROR_SUCCESS;
+    es.pfnCallback = COutputFrame::EditStreamCallback;
+    (void)::SendMessage(m_hwndRichEdit, EM_STREAMIN, SF_TEXT | SF_UNICODE | SFF_SELECTION, (LPARAM)&es);
+
+    // Position view to the end.
+    //
+    (void)::SendMessage(m_hwndRichEdit, WM_VSCROLL, SB_BOTTOM, NULL);
 }
 
 COutputFrame::COutputFrame()
 {
     m_pParentWindow = NULL;
-    m_szHello[0] = L'\0';
+    m_hwndRichEdit = NULL;
 }
 
 COutputFrame::~COutputFrame()
@@ -75,11 +153,9 @@ LRESULT CALLBACK COutputFrame::OutputWndProc(HWND hWnd, UINT message, WPARAM wPa
         }
         break;
 
-    case WM_ERASEBKGND:
-        return 1;
-
-    case WM_PAINT:
-        lRes = pWnd->OnPaint();
+    case WM_SIZE:
+        pWnd->OnSize(wParam, LOWORD(lParam), HIWORD(lParam));
+        lRes = pWnd->DefaultWindowHandler(message, wParam, lParam);
         break;
 
     case WM_NCDESTROY:
