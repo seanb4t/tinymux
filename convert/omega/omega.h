3ec2f16e16d19907767ec1a9b4940e5dab7a3ede
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2010-06-29 12:36:30 +0000

    Start last flatfile convertor. Add t5x version of scanner/parser. t5x grammer doesn't need Merge(); Fix build on FreeBSD. Start command-line parsing. Change ATTRCOUNT to NEXTATTR. Change Write*() to Write(). Rest of adding validation to t5x. Validate that no attribute number is less than A_USER_START. Added parsing and generation of 2.6 default lock.

---
+++ convert/omega/omega.h
@@ -0,0 +1,16 @@
+#ifndef _OMEGA_H_
+#define _OMEGA_H_
+
+#include "autoconf.h"
+
+#include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <vector>
+using namespace std;
+
+char *StringClone(const char *str);
+
+#endif
