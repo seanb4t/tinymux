165c0a25b7dc2ed2ca945b1a873ca1af8d2ef58e
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2010-07-01 13:38:50 +0000

    Start last flatfile convertor. Add t5x version of scanner/parser. t5x grammer doesn't need Merge(); Fix build on FreeBSD. Start command-line parsing. Change ATTRCOUNT to NEXTATTR. Change Write*() to Write(). Rest of adding validation to t5x. Validate that no attribute number is less than A_USER_START. Added parsing and generation of 2.6 default lock.

---
+++ convert/omega/omega.cpp
@@ -0,0 +1,108 @@
+#include "omega.h"
+#include "p6hgame.h"
+#include "t5xgame.h"
+
+// --------------------------------------------------------------------------
+// StringCloneLen: allocate memory and copy string
+//
+char *StringCloneLen(const char *str, size_t nStr)
+{
+    char *buff = (char *)malloc(nStr+1);
+    if (buff)
+    {
+        memcpy(buff, str, nStr);
+        buff[nStr] = '\0';
+    }
+    else
+    {
+        exit(1);
+    }
+    return buff;
+}
+
+// --------------------------------------------------------------------------
+// StringClone: allocate memory and copy string
+//
+char *StringClone(const char *str)
+{
+    return StringCloneLen(str, strlen(str));
+}
+
+void Usage()
+{
+    fprintf(stderr, "omega <options> <infile> <outfile>\n");
+    fprintf(stderr, "Supported options:\n");
+    fprintf(stderr, "  --rt-p6h - Round-trip PennMUSH flatfile\n");
+    fprintf(stderr, "  --rt-t5x - Round-trip TinyMUX flatfile\n");
+}
+
+int main(int argc, const char *argv[])
+{
+    if (4 != argc)
+    {
+        Usage();
+        return 1;
+    }
+
+    FILE *fpin = NULL;
+    FILE *fpout = NULL;
+
+    const int iModeNone  = 0;
+    const int iModeRTP6H = 1;
+    const int iModeRTT5X = 2;
+    int iMode = iModeNone;
+
+    if (strcmp("--rt-p6h", argv[1]) == 0)
+    {
+        iMode = iModeRTP6H;
+    }
+    else if (strcmp("--rt-t5x", argv[1]) == 0)
+    {
+        iMode = iModeRTT5X;
+    }
+    else
+    {
+        Usage();
+        return 1;
+    }
+
+    fpin = fopen(argv[2], "rb");
+    if (NULL == fpin)
+    {
+        fprintf(stderr, "Input file, %s, not found.\n", argv[2]);
+        return 1;
+    }
+    fpout = fopen(argv[3], "wb");
+    if (NULL == fpout)
+    {
+        fclose(fpin);
+        fprintf(stderr, "Output file, %s, not found.\n", argv[3]);
+        return 1;
+    }
+
+    if (iModeRTP6H == iMode)
+    {
+        extern int p6hparse();
+        extern FILE *p6hin;
+        p6hin = fpin;
+        p6hparse();
+
+        g_p6hgame.Validate();
+        g_p6hgame.Write(fpout);
+        p6hin = NULL;
+    }
+    else if (iModeRTT5X == iMode)
+    {
+        extern int t5xparse();
+        extern FILE *t5xin;
+        t5xin = fpin;
+        t5xparse();
+
+        g_t5xgame.Validate();
+        g_t5xgame.Write(fpout);
+        t5xin = NULL;
+    }
+    fclose(fpout);
+    fclose(fpin);
+    return 0;
+}
