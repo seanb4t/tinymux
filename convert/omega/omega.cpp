f0edb4f201200f7f872f81f0bc555d3519c0e579
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2010-07-04 00:34:37 +0000

    Successfully round-trip an older PennMUSH flatfile (1.7.5p0 or after, but before 1.7.7p40). Upgrade path is close. Derefs are slightly off, and some attributes are just in a different order. Add upgrade note to README. Start P6H_GAME::ConvertT5X(). Add location, contents, exits, link, next, parent, owner, zone, pennies, flagwords, and powerwords. Build attr names. Convert names into something compatible with TinyMUX. Add attribute values. Don't upgrade if we already have the new style. Escape fewer characters in strings if flatfile version is 1. Support p6h and t5x password reset. Fix dbpuller.c build error. Move PennMUSH-to-TinyMUX conversion from P6H_GAME to T5X_GAME and add table known attributes. Fix type conversion bug, population of link field, and variable exit indication.

--- convert/omega/omega.cpp
+++ convert/omega/omega.cpp
@@ -34,73 +34,153 @@ void Usage()
     fprintf(stderr, "Supported options:\n");
     fprintf(stderr, "  --rt-p6h - Round-trip PennMUSH flatfile\n");
     fprintf(stderr, "  --rt-t5x - Round-trip TinyMUX flatfile\n");
+    fprintf(stderr, "  --up-p6h - Upgrade pre-1.7.7p40 PennMUSH flatfile\n");
+    fprintf(stderr, "  --cv-p6h - Convert PennMUSH flatfile to TinyMUX\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "  --reset-password - Reset #1 password to 'potrzebie'\n");
 }
 
 int main(int argc, const char *argv[])
 {
-    if (4 != argc)
-    {
-        Usage();
-        return 1;
-    }
-
     FILE *fpin = NULL;
     FILE *fpout = NULL;
 
-    const int iModeNone  = 0;
-    const int iModeRTP6H = 1;
-    const int iModeRTT5X = 2;
-    int iMode = iModeNone;
+    typedef enum
+    {
+        eModeNone,
+        eModeRTP6H,
+        eModeRTT5X,
+        eModeUPP6H,
+        eModeCVP6H,
+    } Mode;
+    bool fResetPassword = false;
+    Mode mode = eModeNone;
 
-    if (strcmp("--rt-p6h", argv[1]) == 0)
+    int iArg = 1;
+    while (iArg < argc)
     {
-        iMode = iModeRTP6H;
+        if (strcmp("--rt-p6h", argv[iArg]) == 0)
+        {
+            mode = eModeRTP6H;
+            iArg++;
+        }
+        else if (strcmp("--rt-t5x", argv[iArg]) == 0)
+        {
+            mode = eModeRTT5X;
+            iArg++;
+        }
+        else if (strcmp("--up-p6h", argv[iArg]) == 0)
+        {
+            mode = eModeUPP6H;
+            iArg++;
+        }
+        else if (strcmp("--cv-p6h", argv[iArg]) == 0)
+        {
+            mode = eModeCVP6H;
+            iArg++;
+        }
+        else if (strcmp("--reset-password", argv[iArg]) == 0)
+        {
+            fResetPassword = true;
+            iArg++;
+        }
+        else
+        {
+            break;
+        }
     }
-    else if (strcmp("--rt-t5x", argv[1]) == 0)
-    {
-        iMode = iModeRTT5X;
-    }
-    else
+
+    if (eModeNone == mode)
     {
         Usage();
         return 1;
     }
 
-    fpin = fopen(argv[2], "rb");
-    if (NULL == fpin)
+    if (iArg + 2 != argc)
     {
-        fprintf(stderr, "Input file, %s, not found.\n", argv[2]);
-        return 1;
-    }
-    fpout = fopen(argv[3], "wb");
-    if (NULL == fpout)
-    {
-        fclose(fpin);
-        fprintf(stderr, "Output file, %s, not found.\n", argv[3]);
+        Usage();
         return 1;
     }
 
-    if (iModeRTP6H == iMode)
+    fpin = fopen(argv[iArg], "rb");
+    if (NULL == fpin)
+    {
+        fprintf(stderr, "Input file, %s, not found.\n", argv[iArg]);
+        return 1;
+    }
+    fpout = fopen(argv[iArg+1], "wb");
+    if (NULL == fpout)
+    {
+        fclose(fpin);
+        fprintf(stderr, "Output file, %s, not found.\n", argv[iArg+1]);
+        return 1;
+    }
+
+    if (eModeRTP6H == mode)
     {
         extern int p6hparse();
         extern FILE *p6hin;
         p6hin = fpin;
         p6hparse();
+        p6hin = NULL;
 
         g_p6hgame.Validate();
+        if (fResetPassword)
+        {
+            g_p6hgame.ResetPassword();
+        }
         g_p6hgame.Write(fpout);
-        p6hin = NULL;
     }
-    else if (iModeRTT5X == iMode)
+    else if (eModeRTT5X == mode)
     {
         extern int t5xparse();
         extern FILE *t5xin;
         t5xin = fpin;
         t5xparse();
+        t5xin = NULL;
 
         g_t5xgame.Validate();
+        if (fResetPassword)
+        {
+            g_p6hgame.ResetPassword();
+        }
         g_t5xgame.Write(fpout);
-        t5xin = NULL;
+    }
+    else if (eModeUPP6H == mode)
+    {
+        extern int p6hparse();
+        extern FILE *p6hin;
+        p6hin = fpin;
+        p6hparse();
+        p6hin = NULL;
+
+        g_p6hgame.Validate();
+        g_p6hgame.Upgrade();
+        g_p6hgame.Validate();
+        if (fResetPassword)
+        {
+            g_p6hgame.ResetPassword();
+        }
+        g_p6hgame.Write(fpout);
+    }
+    else if (eModeCVP6H == mode)
+    {
+        extern int p6hparse();
+        extern FILE *p6hin;
+        p6hin = fpin;
+        p6hparse();
+        p6hin = NULL;
+
+        g_p6hgame.Validate();
+        g_p6hgame.Upgrade();
+        g_p6hgame.Validate();
+        g_t5xgame.ConvertFromP6H();
+        g_t5xgame.Validate();
+        if (fResetPassword)
+        {
+            g_t5xgame.ResetPassword();
+        }
+        g_t5xgame.Write(fpout);
     }
     fclose(fpout);
     fclose(fpin);
