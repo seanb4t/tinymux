07455c042bc239a48ad471aa3525317de37b13eb
ceruleansparks <ceruleansparks@d1b986fa-651c-0410-a323-35a8662cf44d>
2010-12-29 21:08:00 +0000

    Allow attr_access to function on vattr or wildcarded attributes, so that:
    attr_access WIZ-* wizard
    
    ...will work to restrict all attributes beginning with 'WIZ-' to wizards only.
    Required for Cuendillar.

--- mux/src/db.cpp
+++ mux/src/db.cpp
@@ -2469,6 +2469,7 @@ bool atr_get_info(dbref thing, int atr, dbref *owner, int *flags)
         return false;
     }
     atr_decode_LEN(buff, nLen, NULL, thing, owner, flags, &nLen);
+    
     return true;
 }
 
